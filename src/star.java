fe9cda304fe48c94067e5a17e2f02fa1eaafc6c6
yashsharma127 <yashshar.2323@gmail.com>
2023-07-30 21:08:43 +0530

    starting jawa dsa journey

---
+++ src/star.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class star {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n =sc.nextInt();
+        for (int a = 0; a < n; a++) {
+            System.out.println("what the hell");
+        }
+
+        if (n == 5){
+            System.out.println("cuk it");
+        }
+        else {
+            System.out.println("workinnn");
+        }
+    }
+}
